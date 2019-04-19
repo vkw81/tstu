62f4c99de1f8b2596f7645ab61279e609d38841a
vkw81 <vkw@list.ru>
2019-04-19 12:10:41 +0300

    add notif

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -128,4 +128,19 @@ app.on('pageInit', function (page) {
 	</div>');
 });
 
+function initPushwoosh() {
+	var pushwoosh = cordova.require("pushwoosh-cordova-plugin.PushNotification");
 
+  // Should be called before pushwoosh.onDeviceReady
+  document.addEventListener('push-notification', function(event) {
+		var notification = event.notification;
+		// handle push open here
+	});
+  
+	// Initialize Pushwoosh. This will trigger all pending push notifications on start.
+	pushwoosh.onDeviceReady({
+    appid: "3E661-4E835",
+		projectid: "tstu-237409",
+	});
+}
+initPushwoosh();
