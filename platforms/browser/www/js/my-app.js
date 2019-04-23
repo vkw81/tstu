c611e52ca1b041593f1d989e3eaa0b095007854c
vkw81 <vkw@list.ru>
2019-04-23 16:41:05 +0300

    no pw

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -128,25 +128,4 @@ app.on('pageInit', function (page) {
 	</div>');
 });
 
-function initPushwoosh() {
-	var pushwoosh = cordova.require("pushwoosh-cordova-plugin.PushNotification");
 
-  // Should be called before pushwoosh.onDeviceReady
-  document.addEventListener('push-notification', function(event) {
-		var notification = event.notification;
-		// handle push open here
-	});
-  
-	// Initialize Pushwoosh. This will trigger all pending push notifications on start.
-	pushwoosh.onDeviceReady({
-    	appid: "3E661-4E835",
-		projectid: "tstu-237409",
-	});
-}
-document.addEventListener("deviceready", onDeviceReady, false);
-
-// PhoneGap is loaded and it is now safe to make calls PhoneGap methods
-//
-function onDeviceReady() {
-	initPushwoosh();
-}
