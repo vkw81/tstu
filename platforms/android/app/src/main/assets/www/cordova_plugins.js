518316ab84786d165b718c1d56a3d0f0be571426
vkw81 <vkw@list.ru>
2019-04-24 15:00:57 +0300

    v40

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -450,8 +450,8 @@ module.exports.metadata =
   "cordova-plugin-statusbar": "2.1.3",
   "cordova-plugin-vibration": "2.1.6",
   "cordova-plugin-whitelist": "1.2.2",
-  "cordova-support-google-services": "1.2.1",
-  "cordova-plugin-file": "6.0.1"
+  "cordova-plugin-file": "6.0.1",
+  "cordova-support-google-services": "1.3.1"
 };
 // BOTTOM OF METADATA
 });
